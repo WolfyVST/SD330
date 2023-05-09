0a5d87f3235d3de9105953d3969820eef0fdff33
Ricardo Villalobos <ricardo.villalobos1@marist.edu>
2023-05-08 23:06:26 -0400

    FoxBooks2

--- Foxbooks/script.js
+++ Foxbooks/script.js
@@ -1,14 +1,3 @@
-// select the course dropdown menu
-const courseDropdown = document.querySelector('#course-dropdown');
-
-// add options to the course dropdown menu
-for (let course in courses) {
-  const option = document.createElement('option');
-  option.value = course;
-  option.text = course;
-  courseDropdown.add(option);
-}
-
 document.addEventListener('DOMContentLoaded', function() {
   // Load the books data from JSON file
   fetch('data.json')
@@ -17,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     })
     .then(function(data) {
       // Get the list of featured books
-      const featuredBooks = document.getElementById('featured-books');
+      const featuredBooks = document.getElementById('books-list');
       const books = data.books;
 
       // Iterate through the books and add them to the featured books list
@@ -42,31 +31,27 @@ document.addEventListener('DOMContentLoaded', function() {
       }
 
       // Get the list of courses
-      const courses = data.coursesTypes;
-      const courseDropdown = document.getElementById('course-dropdown');
+      const courses = document.getElementById('courses-list');
+      const courseTypes = data.coursesTypes;
 
-      // Iterate through the course types and add them to the courses list and the course dropdown menu
-      for (const courseCode in courses) {
-        if (courses.hasOwnProperty(courseCode)) {
-          const course = courses[courseCode];
+      // Iterate through the course types and add them to the courses list
+      for (const courseCode in courseTypes) {
+        if (courseTypes.hasOwnProperty(courseCode)) {
+          const course = courseTypes[courseCode];
           const li = document.createElement('li');
           const h3 = document.createElement('h3');
           const p = document.createElement('p');
-          const option = document.createElement('option');
 
           h3.textContent = `${courseCode} - ${course.ID}`;
           p.textContent = `Professor: ${course.Professor}, Semester: ${course.semester}`;
-          option.value = courseCode;
-          option.text = courseCode;
 
           li.appendChild(h3);
           li.appendChild(p);
-          coursesList.appendChild(li);
-          courseDropdown.add(option);
+          courses.appendChild(li);
         }
       }
     })
     .catch(function(error) {
-      console.log(`Error loading books data: ${error}`);
+      console.log(`Error loading data: ${error}`);
     });
 });
